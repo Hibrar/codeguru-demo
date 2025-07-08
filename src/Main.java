fcb3cb591efaddd8d80b967dcc9cfc7101ba6254
Hibrar <hibbahibrar00@gmail.com>
2025-07-08 11:47:08 +0100

    Initial commit for CodeGuru demo

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = null;
+        System.out.println("Name length: " + name.length()); // Risk: NullPointerException
+
+        for (int i = 0; i < 1000000; i++) { // Inefficient loop
+            if (i == 999999) {
+                System.out.println("Finished!");
+            }
+        }
+    }
+}
